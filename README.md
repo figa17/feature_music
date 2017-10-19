f05987b37caa87364e33f1c2bde7004cdbe7344f
Felipe González <fel.alfaro@gmail.com>
2017-10-18 23:46:59 -0300

    avances obtención y enriquecimiento

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-spotify_test
+# Spotify Test
+
+## Librerías
++ Spotipy
++ Pandas
+
+## Links
++ [Radiohead’s saddest song](https://www.theverge.com/2017/2/22/14698446/radiohead-most-depressing-song-data-spotify-gloom-index)
++ [Emolex](http://www.saifmohammad.com/WebPages/NRC-Emotion-Lexicon.htm)
---
+++ lyrics.py
@@ -0,0 +1,37 @@
+import re
+import urllib.request
+from bs4 import BeautifulSoup
+
+def get_lyrics(artist, song_title):
+    artist = artist.lower()
+    song_title = song_title.lower()
+    # remove all except alphanumeric characters from artist and song_title
+    artist = re.sub('[^A-Za-z0-9]+', "", artist)
+    song_title = re.sub('[^A-Za-z0-9]+', "", song_title)
+    if artist.startswith("the"):    # remove starting 'the' from artist e.g. the who -> who
+        artist = artist[3:]
+    url = "https://www.azlyrics.com/lyrics/"+artist+"/"+song_title+".html"
+    #https://www.azlyrics.com/lyrics/entershikari/mothership.html
+    #https://www.azlyrics.com/lyrics/entershikari/mothersip.html
+
+    try:
+        content = urllib.request.urlopen(url).read()
+        soup = BeautifulSoup(content, 'html.parser')
+        lyrics = str(soup)
+        # lyrics lies between up_partition and down_partition
+        up_partition = '<!-- Usage of azlyrics.com content by any third-party lyrics provider is prohibited by our licensing agreement. Sorry about that. -->'
+        down_partition = '<!-- MxM banner -->'
+        lyrics = lyrics.split(up_partition)[1]
+        lyrics = lyrics.split(down_partition)[0]
+        lyrics = lyrics.replace('<br>', '').replace('<br/>', '').replace('</br>', '').replace('</div>', '').strip()
+        lyrics = lyrics.rstrip('\n')
+        # elimino caracteres especiales
+
+        return lyrics
+    except Exception as e:
+        #errror
+        return ""
+
+
+print("#"*20)
+print(get_lyrics('Enter Shikari', 'mothership'))
